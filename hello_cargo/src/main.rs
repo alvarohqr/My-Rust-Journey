a155f35a2e3a797f7c0be66f1965bbd6b4e048aa
alvarohqr <alvarohqr@gmail.com>
2023-07-13 18:20:54 -0700

    Hello World with Cargo!

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+ * cargo buil to build
+ * .\target\debug\hello_cargo.exe to execute
+ * or cargo run to compile and execute
+ * cargo check to make it sure it compiles 
+ */ 
+ 

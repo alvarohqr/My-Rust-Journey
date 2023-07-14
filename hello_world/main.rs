a45aaf3b61be5a70198233b9ac30f027ed3f468d
alvarohqr <alvarohqr@gmail.com>
2023-07-13 17:55:31 -0700

    My first Rust program!

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+
+/*
+ * The main function is the first code that runs in every executable Rust program.
+ * In Rust the indent is with 4 spaces, not a tab.
+*/
+fn main(){
+    println!("Hello, world"); //! means to call a macro
+}
